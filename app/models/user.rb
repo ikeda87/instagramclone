f05aa04e9e31b0270fb9c35a963e9d4fecf39045
Hiroshi Ikeda <ikeda87@gmail.com>
2021-01-02 10:05:06 +0000

    index修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,10 @@
 class User < ApplicationRecord
   before_validation { email.downcase! }
   validates :name,  presence: true, length: { maximum: 30 }
-  validates :email, presence: true, length: { maximum: 255 }, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+  validates :email, presence: true, length: { maximum: 250 },
+             format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
   has_secure_password
-  validates :password, presence: true, length: { minimum: 6 }
+  validates :password, presence: true, length: { minimum: 5 }
   has_many :pictures, dependent: :destroy
   has_many :favorites, dependent: :destroy
   has_many :favorite_pictures, through: :favorites, source: :picture
