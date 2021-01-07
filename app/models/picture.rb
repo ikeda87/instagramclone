ccbb008197cea0cf65ef2720d8139dfafcebce83
Hiroshi Ikeda <ikeda87@gmail.com>
2021-01-07 10:48:19 +0000

    空の投稿を修正

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -3,4 +3,5 @@ class Picture < ApplicationRecord
   belongs_to :user
   has_many :favorites, dependent: :destroy
   has_many :favorite_users, through: :favorites, source: :user
+  validates :content, presence: true
 end
